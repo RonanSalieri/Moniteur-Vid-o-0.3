521c4a9cc550445224c9c0f4206cad6a2f0ddcf3
Ronan <99601763+RonanSalieri@users.noreply.github.com>
2022-04-23 15:39:43 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,3 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;port=3306;dbname=stream_monitor;charset=utf8', 'root', 'monaco');
+$bdd = new PDO('mysql:host=localhost;port=3306;dbname=DBNAME;charset=utf8', 'LOGIN', 'PASSWORD');
 ?>
